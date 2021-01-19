cf1e7d4047f09825ffbe85c7f60ebff835894587
Andrew Tsenhlevych <Andrew_Tsenhlevych@epam.com>
2021-01-19 13:30:15 +0200

    add dynamic attribute to the customer item

---
+++ bin/custom/customextension/src/org/training/catalog/dynamic/CustomerDescriptionDynamicAttributeHandler.java
@@ -0,0 +1,22 @@
+package org.training.catalog.dynamic;
+
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+
+
+public class CustomerDescriptionDynamicAttributeHandler implements DynamicAttributeHandler<String, CustomerModel>
+{
+	@Override
+	public String get(CustomerModel model)
+	{
+		return String.format("%s:%s. Order quantity is: %d", model.getName(), model.getContactEmail(), model.getOrders().size());
+	}
+
+	@Override
+	public void set(CustomerModel model, String s)
+	{
+		// read only
+		throw new UnsupportedOperationException();
+	}
+}
+
